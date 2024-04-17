3a07a5e63232582d3ec56830684b7180f1dff675
Mykel68 <olowookeremicheal9568@gmail.com>
2024-04-17 10:32:31 +0100

    remove windows

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -8,12 +8,7 @@ const Hero = () => {
     <div className="hero min-h-screen bg-gradient-to-r from-[#1C2833] to-[#273746]">
       <div className="hero-content flex-col lg:flex-row-reverse px-6 lg:px-0">
         <div
-          className={`pb-10 w-full lg:w-auto ${
-            window.matchMedia("(min-width: 768px) and (max-width: 1023px)")
-              .matches
-              ? "mt-0"
-              : "mt-[-300px]"
-          }`}
+          className="pb-10 w-full lg:w-auto "
           style={{ minWidth: "500px", height: "300px" }}
         >
           <Lottie animationData={Bitcoin} loop={true} />
